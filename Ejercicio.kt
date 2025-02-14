b51df7c237271b13d97f48bed2ce7f80563ade58
Fran <fletgas@g.educaand.es>
2025-02-14 18:09:55 +0100

    funcion pedir libro y data class

--- Ejercicio.kt
+++ Ejercicio.kt
@@ -1,23 +1,41 @@
-sealed class Usuario(open val id: String, open val nombre: String){
+import kotlin.io.path.fileVisitor
 
+sealed class Usuario(open val id: String, open val nombre: String)
+
+
+data class Estudiante(override val id: String, override val nombre: String, val curso: String): Usuario(id, nombre)
+data class Profesor(override val id: String, override val nombre: String, val curso: String): Usuario(id, nombre)
+data class Visitante(override val id: String, override val nombre: String, val curso: String): Usuario(id, nombre)
+
+
+
+data class Libro(val titulo: String, val autor: String, val anioPublicacion: Int)
+data class Revista(val titulo: String, val issue: Int, val anio: Int)
+data class DVD(val titulo: String, val director: String, val anio: Int)
+
+
+
+
+fun pedirLibro(usuario: Usuario, libro: Libro): String{
+    return when (usuario){
+        is Estudiante -> {
+            "El estudiante ${usuario.nombre} puede pedir prestado ${libro.titulo} durante una semana al ser un estudiante"
+        }
+
+        is Profesor -> {
+            "El profesor ${usuario.nombre} puede pedir prestado ${libro.titulo} durante un mes al ser un profesor"
+        }
+
+        is Visitante -> {
+            "El visitante ${usuario.nombre}} no puede pedir prestado ningún libro"
+        }
+    }
 }
 
 
 
-data class Libro(val titulo: String, val autor: String, val anioPublicacion: Int, override val id: String,
-                 override val nombre: String): Usuario(id, nombre){
 
-}
+fun main(){
 
-
-data class Revista(val titulo: String, val issue: Int, val anio: Int, override val nombre: String,
-                   override val id: String): Usuario(id, nombre){
-
-}
-
-
-
-data class DVD(val titulo: String, val direcctor: String, val anio: Int, override val id: String,
-               override val nombre: String): Usuario(id, nombre){
 
 }
